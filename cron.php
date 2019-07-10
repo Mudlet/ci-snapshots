813344222d6f4485ef2d32d28f5356548e315cc8
Fae <itsthefae@gmail.com>
2019-07-09 22:41:34 -0700

    add cron task for Travis CI IP changes.

--- cron.php
+++ cron.php
@@ -122,3 +122,80 @@ if( $dirSize > MAX_CAPACITY_BYTES && MAX_CAPACITY_DELETE_OLDEST == true ) {
     }
 }
 
+
+// Check for IP Safe-List updates.
+//  For now we check for travis IPs using the method 
+//  documented here:  https://docs.travis-ci.com/user/ip-addresses/
+//  Would like to find a method we could use for AppVeyor as well.
+$ipListFile = $ScriptPath . '/ip_list';
+function setIpListData($data) {
+    global $ipListFile;
+    $io = fopen($ipListFile, 'w');
+    foreach($data as $idx => $lineparts) {
+        $ip = $lineparts[0]; 
+        $comment = $lineparts[1];
+        if(!empty($comment)) {
+            $comment = "\t# {$comment}";
+        }
+        $line = "{$ip}\t1{$comment}\n";
+        fwrite($io, $line);
+    }
+    fclose($io);
+}
+function getIpListData() {
+    global $ipListFile;
+    $data = array();
+    $io = fopen($ipListFile, 'r');
+    while($line = fgets($io)) {
+        preg_match('/([0-9a-f\.:]+)\t1(?:\t#\s*(.+))?/i', $line, $m);
+        if(count($m) == 2) {
+            $data[] = array($m[1], '');
+        }
+        if(count($m) == 3) {
+            $data[] = array($m[1], $m[2]);
+        }
+    }
+    fclose($io);
+    return $data;
+}
+function getTravisIps() {
+    $data = array();
+    $io = popen('dig +short nat.travisci.net | sort', 'r');
+    while($line = fgets($io)) {
+        $data[] = trim($line);
+    }
+    pclose($io);
+    return $data;
+}
+function filterIpsInSafeList($ipArray) {
+    $ipListData = getIpListData();
+    $ipList = array();
+    foreach($ipListData as $idx => $set) {
+        $ipList[] = $set[0];
+    }
+    return array_diff($ipArray, $ipList);
+}
+
+if( is_file($ipListFile) && is_readable($ipListFile) && is_writable($ipListFile) ) {
+    $ipListDataArray = getIpListData();
+    $ips = filterIpsInSafeList( getTravisIps() );
+    foreach($ips as $idx => $ip) {
+        $dObj = new DateTime();
+        $date = $dObj->format('Y-m-d H:i T');
+        $comment = "Travis CI address added by Cron task on {$date}";
+        $ipListDataArray[] = array($ip, $comment);
+    }
+    
+    if(count($ips) > 0) {
+        echo "New Travis CI IPs added:\n";
+        foreach($ips as $idx => $ip) {
+            echo ' - ' . $ip . "\n";
+        }
+        echo "\n";
+    }
+    
+    setIpListData( $ipListDataArray );
+} else {
+    echo 'Error - IP Safe-List file was not found!';
+}
+
